08256487c4fa174bde0eff9dba8aa1c7837f4733
Tsvetomir <tsvetomirstoev8@gmail.com>
2021-07-06 16:19:10 +0300

    Connected picture Api

---
+++ src/api/unsplash.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: { Authorization: "Client-ID OTMdeN-kagM6uS-joFtOowrdRoGzYwwciq8wqLGrISc"}
+});
